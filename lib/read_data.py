de2b3ef0aa114632561fcae5223f7980641adc4e
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-10-14 13:01:10 +0200

    setup initial

--- lib/read_data.py
+++ lib/read_data.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 
 class DataSource:
-    """Used to extract data from a json file."""
+    """Used to extract data from a json file"""
 
     def __init__(self, path: str) -> None:
         self.path = path

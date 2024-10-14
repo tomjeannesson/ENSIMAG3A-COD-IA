2446d33cd0781b0728d8abed7231af5711b81575
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-10-14 13:13:14 +0200

    setup initial

--- lib/read_data.py
+++ lib/read_data.py
@@ -6,6 +6,6 @@ class DataSource:
     """Used to extract data from a json file"""
 
     def __init__(self, path: str) -> None:
-        self.path = pat h
+        self.path = path
         with Path.open(path) as f:
             self.raw_data = json.load(f)

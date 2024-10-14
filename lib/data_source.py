99d89547e6639850f36ad26bb827588347034924
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-10-14 17:24:55 +0200

    extractor

--- lib/data_source.py
+++ lib/data_source.py
@@ -1,5 +1,3 @@
-#! /usr/bin/env python3
-
 import json
 from collections.abc import Callable
 from pathlib import Path
---
+++ lib/extractor.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+import pandas as pd
+
+
+@dataclass
+class DataPoint:
+    """Represents one instance of data."""
+
+    raw: float
+    rank: int
+    max: float
+
+
+class Extractor:
+    """Transforms a list of DataFrames into a list of data stats."""
+
+    def __init__(self, dataframe_list: list[pd.DataFrame]) -> None:
+        self.dataframes = dataframe_list
+        self.stat_labels = ["total_points"]
+
+    def extract_athlete(self, name: str) -> list[dict[str, DataPoint]]:
+        """Transforms a list of DataFrames into a list of data stats according to the athlete axis."""
+        res = []
+        for dataframe in self.dataframes:
+            row = dataframe.loc[name]
+            stats = {}
+            for stat in self.stat_labels:
+                column = dataframe[stat]
+                raw = row[stat]
+                rank = len(column[column > raw]) + 1
+                stats[stat] = DataPoint(
+                    raw=raw,
+                    rank=rank,
+                    max=column.max(),
+                )
+            res.append(stats)
+        return res
+
+    def extract_run(self) -> None:
+        """Transforms a list of DataFrames into a list of general race stats."""

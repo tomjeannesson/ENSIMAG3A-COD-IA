890c3c76f62e11dd18191d78f9fdff5be6c5ec9f
Julian Coux <Julian.coux@grenoble-inp.org>
2024-10-24 12:44:12 +0200

    merge

--- lib/extractor.py
+++ lib/extractor.py
@@ -1,3 +1,4 @@
+import statistics
 from dataclasses import dataclass
 
 import pandas as pd
@@ -77,6 +78,58 @@ class Extractor:
                     )
                 )
             stats[stat] = list_stat
+        self.last_extract = ("Athlete", name, stats)
+        return stats
+
+    def extract_athlete_v2(self, name: str) -> dict[str, dict]:
+        """Transforms a list of DataFrames into a dictionary of a table of data stats according to the athlete axis."""
+        stats = {}
+
+        def create_dico_stat_athlete(list_stat: float, is_raw: int) -> dict[str, float]:
+            try:
+                quantiles = statistics.quantiles(list_stat, n=4)
+                stdev = statistics.stdev(list_stat)
+            except statistics.StatisticsError:
+                quantiles = [list_stat[0], list_stat[0], list_stat[0]]
+                stdev = 0
+            return {
+                "min": min(list_stat) if is_raw > 0 else max(list_stat),
+                "max": max(list_stat) if is_raw > 0 else min(list_stat),
+                "mean": sum(list_stat) / len(list_stat),
+                "std": stdev,
+                "q1": quantiles[0],
+                "q2": quantiles[2],
+            }
+
+        for stat in self.stat_labels:
+            dico_data = {}
+            list_stat = []
+            list_stat_raw = []
+            list_stat_rank = []
+            for dataframe in self.dataframes:
+                try:
+                    row = dataframe.loc[name]
+                except KeyError:
+                    continue
+                column = dataframe[stat]
+                raw = row[stat]
+                rank = len(column[column > raw]) + 1
+                list_stat.append(
+                    DataAthlete(
+                        athlete=name,
+                        metadata=dataframe.metadata,
+                        raw=raw,
+                        rank=rank,
+                        max=column.max(),
+                    )
+                )
+                list_stat_raw.append(raw)
+                list_stat_rank.append(rank)
+
+            dico_data["data"] = list_stat
+            dico_data["raw"] = create_dico_stat_athlete(list_stat=list_stat_raw, is_raw=1)
+            dico_data["rank"] = create_dico_stat_athlete(list_stat=list_stat_rank, is_raw=0)
+            stats[stat] = dico_data
         self.last_extract = ("Athlete", name, stats)
         return stats
 

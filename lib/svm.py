d85c58793347894d0e3622f7783936b115dbf2ec
Tom JEANNESSON <tomjeannesson@gmail.com>
2024-12-12 08:44:06 +0100

    Minor commit @ Jeu 12 déc 2024 08:23:23 CET

---
+++ lib/svm.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from data_source import DataSource
+
+source = DataSource("data.json")
+qualifications = source.filter([["WC"], ["M"], [], [], ["Q"]], "include")
+dataframe = qualifications[0]
+
+dataframe = dataframe.drop(
+    [
+        "top_air_trick",
+        "country",
+        "bottom_air_trick",
+        "first_name",
+        "last_name",
+        "birth_year",
+        "fis_id",
+        "bib",
+        "tie",
+    ],
+    axis=1,
+)
+for _df in qualifications[1:]:
+    _df = _df.drop(
+        [
+            "top_air_trick",
+            "country",
+            "bottom_air_trick",
+            "first_name",
+            "last_name",
+            "birth_year",
+            "fis_id",
+            "bib",
+            "tie",
+        ],
+        axis=1,
+    )
+    dataframe = pd.concat([dataframe, _df], axis=0)
+dataframe = dataframe.apply(
+    lambda col: (abs(col) - abs(col).min()) / (abs(col).max() - abs(col).min()),
+    axis=0,
+)
